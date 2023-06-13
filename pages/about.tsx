4311b0b8e4f3ecd0ee57d53f3d24dfcfa37ba43f
Hieu Nguyen <nhocproaoe@gmail.com>
2023-06-14 00:12:14 +0700

    done 02-06 getStaticProps ssg

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,25 @@
+import { log } from "console";
+import { useRouter } from "next/router";
 import * as React from "react";
 
-export interface IAboutPageProps {}
+export interface IAboutPageProps {
+  name: string;
+}
 
 export default function App(props: IAboutPageProps) {
-  return <div>Hello from about page</div>;
+  const router = useRouter();
+  console.log("about query", router.query);
+
+  // React.useEffect(() => {
+  //   //must check if router query is undefined cause at first time it will be undefined
+  // }, []);
+  return <div>Hello from about page </div>;
 }
+
+// export async function getServerSideProps() {
+//   return {
+//     props: {
+//       name: "hello",
+//     }, //this will make component not as aso ( must down the js file to execute )
+//   };
+// }
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,52 @@
+import { GetStaticProps, GetStaticPropsContext } from "next";
 import * as React from "react";
 
-export interface IPostsProps {}
-
-export default function Posts(props: IPostsProps) {
-  return <div>hello from posts page</div>;
+interface Post {
+  id: string;
+  title: string;
+  imageUrl: string;
+  description: string;
+  author: string;
 }
+
+interface PageProps {
+  posts: Post[];
+}
+
+export default function Posts(props: PageProps) {
+  console.log("size", props.posts.length);
+
+  return (
+    <div>
+      hello from posts page
+      {props.posts.map((i, index) => {
+        return (
+          <li key={index}>
+            {i.id}-{i.title}
+          </li>
+        );
+      })}
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps<PageProps> = async (
+  context: GetStaticPropsContext
+) => {
+  //this run from server(nodejs server)
+  const response = await fetch(
+    "https://js-post-api.herokuapp.com/api/posts/?_page=1"
+  );
+
+  const data = await response.json();
+
+  return {
+    //build o server-side
+    //run luc build time
+    //ko duoc dung chung getServerSideProps neu dung chung se loi
+    //neu dung ssr thi bo het
+    props: {
+      posts: [...data.data],
+    },
+  };
+};
