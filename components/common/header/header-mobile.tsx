2eb9c6033649ff0c92f8ddac024006b6ba7edcf4
Hieu Nguyen <nhocproaoe@gmail.com>
2023-06-17 16:08:14 +0700

    done header for mobile

--- components/common/header/header-mobile.tsx
+++ components/common/header/header-mobile.tsx
@@ -1,8 +1,70 @@
-import * as React from "react";
+import { DehazeOutlined } from "@mui/icons-material";
+import {
+  Container,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Link as MuiLink,
+  Stack,
+} from "@mui/material";
+import Button from "@mui/material/Button";
+import Drawer from "@mui/material/Drawer";
 import { Box } from "@mui/system";
+import clsx from "clsx";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import * as React from "react";
+import { ROUTE_LIST } from "./routes";
 
 export interface IHeaderMobileProps {}
 
 export default function HeaderMobile(props: IHeaderMobileProps) {
-  return <Box display={{ xs: "block", md: "none" }}>Header mobile</Box>;
+  const [showDrawer, setShowDrawer] = React.useState<boolean>(false);
+
+  const toggleDrawer = () => setShowDrawer(!showDrawer);
+
+  const router = useRouter();
+
+  return (
+    <Box display={{ xs: "block", md: "none" }}>
+      <Container>
+        <Stack direction={"row"} justifyContent={"flex-end"}>
+          <React.Fragment key={"right"}>
+            <Button onClick={toggleDrawer}>
+              <DehazeOutlined />
+            </Button>
+            <Drawer anchor={"right"} open={showDrawer} onClose={toggleDrawer}>
+              <List>
+                {ROUTE_LIST.map((route) => (
+                  <ListItem key={route.path} onClick={toggleDrawer}>
+                    <ListItemButton>
+                      <Link
+                        key={route.path}
+                        href={route.path}
+                        legacyBehavior
+                        passHref
+                      >
+                        <MuiLink
+                          sx={{
+                            ml: 2,
+                          }}
+                          underline="none"
+                          className={clsx({
+                            active: router.pathname === route.path,
+                          })}
+                        >
+                          <ListItemText primary={route.label} />
+                        </MuiLink>
+                      </Link>
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+              </List>
+            </Drawer>
+          </React.Fragment>
+        </Stack>
+      </Container>
+    </Box>
+  );
 }
