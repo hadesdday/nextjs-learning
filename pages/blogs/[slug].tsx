838b25d424a1ba9474c7ab2fa47b2566bd962155
Hieu Nguyen <nhocproaoe@gmail.com>
2023-06-19 15:55:38 +0700

    done 05-08 complete blog details page

---
+++ pages/blogs/[slug].tsx
@@ -0,0 +1,70 @@
+import { MainLayout } from "@/components/layout";
+import { Blog } from "@/models";
+import { getPostList } from "@/utils/blogs";
+import { Box, Container, Typography } from "@mui/material";
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
+import Link from "next/link";
+import rehypeDocument from "rehype-document";
+import rehypeFormat from "rehype-format";
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
+
+export interface IBlogDetailsProps {
+  blog: Blog;
+}
+
+export default function BlogDetailsPage({ blog }: IBlogDetailsProps) {
+  if (!blog) return null;
+
+  const { title, author, description, mdContent, htmlContent } = blog;
+
+  return (
+    <Box sx={{ pt: 5 }}>
+      <Container>
+        <Typography variant="h4">{title}</Typography>
+        <Typography variant="h5">{author?.name}</Typography>
+        <div dangerouslySetInnerHTML={{ __html: htmlContent || "" }}></div>
+      </Container>
+    </Box>
+  );
+}
+
+BlogDetailsPage.Layout = MainLayout;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await getPostList();
+
+  return {
+    paths: response.map((i: Blog) => ({
+      params: { slug: i.slug },
+    })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<IBlogDetailsProps> = async (
+  context: GetStaticPropsContext
+) => {
+  const slug = context.params?.slug;
+  const blogList = await getPostList();
+  let blog = blogList.find((x) => x.slug == slug);
+
+  if (!blog) return { notFound: true };
+
+  const file = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeDocument, { title: blog.title })
+    .use(rehypeFormat)
+    .use(rehypeStringify)
+    .process(blog.mdContent || "");
+
+  blog.htmlContent = String(file);
+  return {
+    props: {
+      blog,
+    },
+  };
+};
