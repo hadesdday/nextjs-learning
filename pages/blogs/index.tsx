ded37c2da03a4cf9b843038f0cb403d16b1bbb38
Hieu Nguyen <nhocproaoe@gmail.com>
2023-06-20 13:40:57 +0700

    up

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,4 +1,5 @@
 import { BlogItem } from "@/components/blog";
+import { Seo } from "@/components/common";
 import { MainLayout } from "@/components/layout";
 import { Blog } from "@/models";
 import { getPostList } from "@/utils/blogs";
@@ -12,9 +13,18 @@ interface BlogListProps {
 export default function BlogListPage({ blogs }: BlogListProps) {
   return (
     <Box>
+      <Seo
+        data={{
+          title: `Blogs | Next.js Blog`,
+          description: "Shared blogs for everyone",
+          url: `${process.env.HOST_URL}/blogs/`,
+          thumbnailUrl:
+            "https://raw.githubusercontent.com/vercel/serve/main/media/banner.png",
+        }}
+      />
       <Container sx={{ pt: { xs: 2, sm: 4 } }}>
         <Typography variant="h4" fontWeight={"bold"} py={4}>
-          Blog
+          Blogs
         </Typography>
         <Box component={"ul"} sx={{ listStyleType: "none", p: 0 }}>
           {blogs.map((blog, index) => {
